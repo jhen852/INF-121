b7a030bc5b041f48156001bc279abe5085b8468a
jhen852 <jhenny.callapa.quispe@gmail.com>
2020-08-31 11:34:31 -0400

    versio 1

---
+++ Ej1POO/Ej1POO/Cuaderno.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JHENNY
+ * Date: 31/08/2020
+ * Time: 10:56 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ * 
+ * CLASE #1
+ */
+using System;
+
+namespace Ej1POO
+{
+	/// <summary>
+	/// Description of Cuaderno.
+	/// </summary>
+	public class Cuaderno
+	{
+		//Atributos
+		private String marca,tamaño,tipo;
+		private int precio;
+		
+		//Metodos
+		// datos son parametros que vamos a mandar
+		public void leer(){//Realiza una tarea especifica para menjar objetos
+			marca=Console.ReadLine();
+			tamaño=Console.ReadLine();
+			tipo=Console.ReadLine();
+			//Aqui estamos castiando
+			precio=int.Parse(Console.ReadLine());
+		}
+		
+		public void mostrar(){
+			Console.Write(marca+" "+tamaño+" "+tipo+" "+precio);
+		}
+		
+	}
+}
---
+++ Ej1POO/Ej1POO/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 31/08/2020
+ * Time: 10:55 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej1POO
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// creacion Obejtos
+			//NombreClase objeto= new NombreClase ();
+			
+			Cuaderno A= new Cuaderno(); // un objeto
+			
+			A.leer();
+			A.mostrar();
+			
+			// otro objeto
+			
+			Cuaderno Z= new Cuaderno();
+			
+			Console.WriteLine("Otro Objeto");
+			
+			Z.leer();
+			Z.mostrar();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
